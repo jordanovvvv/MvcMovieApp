fe133c9b13853b7b99a3fede590468505e9eca2b
jordanovvvv <jordanovsimeon1@gmail.com>
2023-11-14 15:24:58 +0100

    Added ImageUrl

---
+++ MvcMovie/Migrations/20231114140306_ImageUrl.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    public partial class ImageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Movies",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Movies");
+        }
+    }
+}
